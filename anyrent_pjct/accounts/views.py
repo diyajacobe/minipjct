0ad5369666bdab4bbec0be77773147362ab95d12
diyajacobe <diyamoljacob2023a@mca.ajce.in>
2022-09-18 14:24:23 +0530

    new

--- anyrent_pjct/accounts/views.py
+++ anyrent_pjct/accounts/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Home(request):
+    return render(request,"index.html")
+
+from django.contrib import messages, auth
+from django.shortcuts import render, redirect
+# from .models import Account
+from django.contrib.auth import authenticate
+
+
+
+
+def login(request):
+    # if request.method == 'POST':
+    #     email=request.POST['email']
+    #     password=request.POST['password']
+    #     user=authenticate(email=email, password=password)
+    #     print(email)
+    #     print(password)
+    #     print(user)
+    #     if user is not None:
+    #         auth.login(request, user)
+    #         messages.success(request, 'you are logged in')
+    #         # request.session['email']=email
+    #         # request.session['fname']=user.fname
+    #         # # store user details in session
+    #         # request.session['district']=user.district
+    #         return redirect('home')
+    #     else:
+    #         print("demo")
+    #         messages.error(request, 'invalid login credentials')
+    #         return redirect('register')
+    return render(request, 'signin.html')
+
+def register(request):
+    # if request.method == 'POST':
+    #     email=request.POST['email']
+    #     password=request.POST['password']
+    #     lname = request.POST['lname']
+    #     fname=request.POST['fname']
+    #
+    #     phone_number=request.POST['tel']
+    #     print(email,password,fname,lname,phone_number)
+    #     if Account.objects.filter(email=email).exists():
+    #         messages.error(request, 'email already exists')
+    #         return redirect('register')
+    #     # elif Account.objects.filter(fname=fname).exists():
+    #     #     messages.error(request, 'username already exists')
+    #     #     return redirect('register')
+    #     else:
+    #         user=Account.objects.create_user(email=email, password=password, fname=fname, lname=lname, phone_number=phone_number)
+    #         user.save()
+    #         messages.success(request, 'you are registered')
+    #         return redirect('/login')
+    return render(request, 'signup.html')
--- anyrent_pjct/anyrent_pjct/settings.py
+++ anyrent_pjct/anyrent_pjct/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'anyrent_pjct.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,6 +73,8 @@ WSGI_APPLICATION = 'anyrent_pjct.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+AUTH_USER_MODEL='accounts.Account'
 
 DATABASES = {
     'default': {
@@ -118,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
