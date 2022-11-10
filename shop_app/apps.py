6e9f623b9d9d4f242c0db52d305a3ffda93711c7
diyajacobe <diyamoljacob2023a@mca.ajce.in>
2022-11-10 21:53:03 +0530

    second review completed

--- shop_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShopAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shop_app'
--- shop_app/context_processors.py
+++
@@ -1,7 +0,0 @@
-from products.models import Category
-
-
-def menu_links(request):
-    links = Category.objects.all()
-    return dict(links=links)
-
