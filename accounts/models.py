da4fb67dc9fd178bf0c6dc8cf713c508dab0233d
Diya Jacob <97578901+diyajacobe@users.noreply.github.com>
2022-11-22 20:53:41 +0530

    registraion details

---
+++ accounts/models.py
@@ -0,0 +1,76 @@
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+# Create your models here.
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, fname, lname, email,phone_number, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        if not lname:
+            raise ValueError('User must have an username')
+
+        user = self.model(
+            email=self.normalize_email(email),
+                        lname=lname,
+                        fname=fname,
+                        phone_number=phone_number,
+
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, fname, email, lname, password,  phone_number,):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            fname=fname,
+            password=password,
+            lname=lname,
+            phone_number=phone_number,
+
+        )
+        user.is_admin = True
+        user.is_active = True
+        user.is_staff = True
+        user.is_superadmin = True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser,PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    fname = models.CharField(max_length=100, blank=True, null=True,editable=False)
+    lname = models.CharField(max_length=100, blank=True, null=True,editable=False)
+    email = models.EmailField(max_length=100, unique=True, editable=False)
+    phone_number = models.BigIntegerField(default=0, editable=False)
+
+    # required
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['fname', 'lname',  'phone_number',]
+
+    objects = MyAccountManager()
+
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
+
+
---
+++ accounts/views.py
@@ -0,0 +1,180 @@
+
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages, auth
+from django.http import JsonResponse,HttpResponse
+from django.shortcuts import render, redirect
+from .models import Account
+from django.contrib.auth import authenticate
+
+
+#email verification import files
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.core.mail import send_mail
+
+
+# Create your views here.
+
+
+
+
+
+
+def login(request):
+    if request.method == 'POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        user=authenticate(email=email, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            messages.success(request, 'you are logged in')
+            request.session['email']=email
+            if user.is_admin:
+                return redirect('admin/')
+            else:
+                return redirect('home')
+        else:
+            messages.error(request, 'invalid login credentials')
+            return redirect('login')
+    return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        fname=request.POST['fname']
+        lname=request.POST['lname']
+        phone_number=request.POST['tel']
+        if Account.objects.filter(email=email).exists():
+            messages.success(request, 'email already exists')
+            return redirect('register')
+        else:
+            user=Account.objects.create_user(email=email, password=password, fname=fname, lname=lname,  phone_number=phone_number)
+            user.save()
+            messages.success(request, 'Thank you for registering with us.')
+            messages.success(request, 'Please verify your email for login!')
+
+            #code for email verification also check validate function
+            current_site = get_current_site(request)
+            message = render_to_string('account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+
+            send_mail(
+                'Please activate your account',
+                message,
+                'anyrentplatfrom@gmail.com',
+                [email],
+                fail_silently=False,
+            )
+
+            return redirect('/login/?command=verification&email=' + email)
+        # return redirect('/login')
+    return render(request, 'register.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+
+
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+
+            # Reset password email
+
+            current_site = get_current_site(request)
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+
+            send_mail(
+                'Please activate your account',
+                message,
+                'sankartstore@gmail.com',
+                [email],
+                fail_silently=False,
+            )
+
+            messages.success(request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist!')
+            return redirect('forgotPassword')
+    return render(request, 'accounts/lost-password.html')
+
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Please reset your password')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'This link has been expired!')
+        return redirect('login')
+
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')
+
+
+
+
+    #filter Data
+
+# def filter_data(request):
+#     categories=request.GET.getlist('color[]')
+#     brands = request.GET.getlist('color[]')
+#     allProducts = Product.objects.all().order_by('-id')
+#     t=render_to_string('ajax/product-list',{'data':allProducts})
+#     return JsonResponse({'data':t})
+
